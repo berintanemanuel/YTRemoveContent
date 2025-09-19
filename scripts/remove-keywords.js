65b5561f445939145d763f13336c85c669143488
Nati404 <natiberintan3@gmail.com>
2025-09-19 23:58:32 +0300

    Add functionality to remove all videos from the homepage that contain a keyword in an active group in the keywords.json file

--- scripts/remove-keywords.js
+++ scripts/remove-keywords.js
@@ -1,4 +1,4 @@
-// Removes all videos that contain, in their title, one of the keywords that are in an active group in the keywords.js file"
+// Removes all videos that contain, in their title, one of the keywords that are in an active group in the keywords.js file
 const url = chrome.runtime.getURL('../data/keywords.json');
 
 async function getKeywordsData(){
@@ -7,8 +7,35 @@ async function getKeywordsData(){
     return data;
 }   
 
-async function removeVideosWithKeywordInTitle(){
-    const keywordsData = await getKeywordsData();
+const removeAllVideosContainingKeywordInHomePage = (word) => {
+    document.querySelectorAll("a>span.yt-core-attributed-string").forEach(elem => {
+        const content = elem.textContent.toLowerCase();
+        if(content.indexOf(word) !== -1){
+            // We have found a video with a title that contains this keyword so we remove it
+            while(elem != null && elem.tagName.toLowerCase() != 'ytd-rich-item-renderer' && elem.tagName != 'html'){
+            elem = elem.parentNode;
+            }
+            if(elem.tagName.toLowerCase() != "ytd-rich-item-renderer") return; // Make sure we do not delete something else in case the correct tag was not found
+            elem.remove();
+        }
+    });
 }
 
-removeVideosWithKeywordInTitle();
+async function removeVideosWithKeywordsInTitle(){
+    const keywordsData = await getKeywordsData();
+    for(const groupName in keywordsData){
+        if(keywordsData[groupName].active === false) continue;
+        for(const word of keywordsData[groupName].words){
+            removeAllVideosContainingKeywordInHomePage(word);
+        }
+    }
+}
+
+const observerKeyword = new MutationObserver(removeVideosWithKeywordsInTitle);
+
+observerKeyword.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
+removeVideosWithKeywordsInTitle();
