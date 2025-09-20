10eca446e1bc722c1e417d1b84aaeb880d7f748b
Nati404 <natiberintan3@gmail.com>
2025-09-20 14:51:24 +0300

    Implement functionality to remove videos from given creators in the Results Page. Change string matching from partial matching to exact matching to avoid overdelete of channels.

--- scripts/remove-creators.js
+++ scripts/remove-creators.js
@@ -15,7 +15,8 @@ const removeAllVideosByCreatorsInHomePage = (creator) => {
     }
     document.querySelectorAll("a.yt-core-attributed-string__link").forEach(elem => {
         const content = elem.textContent.toLowerCase();
-        if(content !== creator){
+        // Needs to be exact matching in order to not overdelete channels (Channels that may contain another channels name in their name)
+        if(content === creator){
             // We have found a video from this creator so we remove it
             try{
                 while(elem != null && elem.tagName.toLowerCase() != 'ytd-rich-item-renderer' && elem.tagName != 'html'){
@@ -30,12 +31,35 @@ const removeAllVideosByCreatorsInHomePage = (creator) => {
     });
 }
 
+const removeAllVideosByCreatorsInResultsPage = (creator) => {
+    try{
+        creator = creator.toLowerCase();
+    } catch(err){
+        return;
+    }
+    document.querySelectorAll("yt-formatted-string.ytd-channel-name>a").forEach(elem => {
+        const content = elem.textContent.toLowerCase();
+        if(creator === content){
+            try{
+                while(elem != null && elem.tagName.toLowerCase() != 'ytd-video-renderer' && elem.tagName != 'html'){
+                elem = elem.parentNode;
+                }
+                if(elem.tagName.toLowerCase() != "ytd-video-renderer") return; // Make sure we do not delete something else in case the correct tag was not found
+                elem.remove();
+            } catch(err){
+                return;
+            }
+        }
+    });
+}
+
 async function removeVideosByGivenCreators(){
     const creatorsData = await getCreatorsData();
     for(const groupName in creatorsData){
         if(creatorsData[groupName].active === false) continue;
         for(const creator of creatorsData[groupName].creators){
             removeAllVideosByCreatorsInHomePage(creator);
+            removeAllVideosByCreatorsInResultsPage(creator);
         }
     }
 }
