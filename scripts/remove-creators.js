7b9a35f38667656dcd06c387929818820ce1e81d
Nati404 <natiberintan3@gmail.com>
2025-09-20 14:04:46 +0300

    Implement functionality to remove videos from given creators in the Home Page

---
+++ scripts/remove-creators.js
@@ -0,0 +1,50 @@
+// Removes all videos that are from one of the creators that are in an active group in the creators.json file
+const urlCreators = chrome.runtime.getURL('../data/creators.json');
+
+async function getCreatorsData(){
+    const response = await fetch(urlCreators);
+    const data = await response.json();
+    return data;
+}   
+
+const removeAllVideosByCreatorsInHomePage = (creator) => {
+    try{
+        creator = creator.toLowerCase();
+    } catch(err){
+        return;
+    }
+    document.querySelectorAll("a.yt-core-attributed-string__link").forEach(elem => {
+        const content = elem.textContent.toLowerCase();
+        if(content !== creator){
+            // We have found a video from this creator so we remove it
+            try{
+                while(elem != null && elem.tagName.toLowerCase() != 'ytd-rich-item-renderer' && elem.tagName != 'html'){
+                elem = elem.parentNode;
+                }
+                if(elem.tagName.toLowerCase() != "ytd-rich-item-renderer") return; // Make sure we do not delete something else in case the correct tag was not found
+                elem.remove();
+            } catch(err){
+                return;
+            }
+        }
+    });
+}
+
+async function removeVideosByGivenCreators(){
+    const creatorsData = await getCreatorsData();
+    for(const groupName in creatorsData){
+        if(creatorsData[groupName].active === false) continue;
+        for(const creator of creatorsData[groupName].creators){
+            removeAllVideosByCreatorsInHomePage(creator);
+        }
+    }
+}
+
+const observerCreator = new MutationObserver(removeVideosByGivenCreators);
+
+observerCreator.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
+removeVideosByGivenCreators();
